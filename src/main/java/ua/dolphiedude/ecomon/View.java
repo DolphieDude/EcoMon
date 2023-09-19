4da9e94c24c8c426cf8d14ab0c1518c5c46eb8c0
DolphieDude <andreycherevat@gmail.com>
2023-09-19 22:46:41 +0200

    connect to DB and bind Facility entity

--- src/main/java/ua/dolphiedude/ecomon/View.java
+++ src/main/java/ua/dolphiedude/ecomon/View.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
 
 @Route("")
@@ -15,6 +17,7 @@ public class View extends VerticalLayout {
     private TextField activity = new TextField("Activity");
     private TextField ownership = new TextField("Ownership");
     private TextField ecologicalDescription = new TextField("Ecological Description");
+    private Binder binder = new Binder<>(Facility.class);
 
 
     public View(FacilityRepository repository) {
@@ -28,6 +31,18 @@ public class View extends VerticalLayout {
         Button addButton = new Button("Add");
         addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         layout.add(name, activity, ownership, ecologicalDescription, addButton);
+        binder.bindInstanceFields(this);
+
+        addButton.addClickListener(add -> {
+            try {
+                Facility facility = new Facility();
+                binder.writeBean(facility);
+                repository.save(facility);
+                binder.readBean(new Facility());
+            } catch (ValidationException e) {
+                throw new RuntimeException(e);
+            }
+        });
         return layout;
     }
 }
