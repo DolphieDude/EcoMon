e1d3d51277fa93042ee721400e53372bf4230174
DolphieDude <andreycherevat@gmail.com>
2023-10-06 01:58:41 +0200

    add Lombok dependency and implement for @Data in all entities

--- src/main/java/ua/dolphiedude/ecomon/substance/Substance.java
+++ src/main/java/ua/dolphiedude/ecomon/substance/Substance.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
-public class Substance {
+@Data public class Substance {
     @Id
     @GeneratedValue
     @Column(name = "id_substance")
@@ -22,35 +23,4 @@ public class Substance {
         return name;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUnits() {
-        return units;
-    }
-
-    public void setUnits(String units) {
-        this.units = units;
-    }
-
-    public Double getGdk() {
-        return gdk;
-    }
-
-    public void setGdk(Double gdk) {
-        this.gdk = gdk;
-    }
 }
