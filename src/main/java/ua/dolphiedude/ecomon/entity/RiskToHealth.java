c995a1f2b59bf84fee4020b89d6aab5b63148021
DolphieDude <andreycherevat@gmail.com>
2023-11-28 00:20:56 +0100

    add RiskToHealth entity and add additional attributes for Substance

---
+++ src/main/java/ua/dolphiedude/ecomon/entity/RiskToHealth.java
@@ -0,0 +1,41 @@
+package ua.dolphiedude.ecomon.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "risk_to_health")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RiskToHealth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "emission_id", nullable = false, referencedColumnName = "id")
+    private Emission emission;
+
+    @Size(max = 45)
+    @Column(name = "non_carcinogen_message", length = 45)
+    private String nonCarcinogenMessage;
+
+    @Size(max = 45)
+    @Column(name = "carcinogen_message", length = 45)
+    private String carcinogenMessage;
+
+    @Column(name = "non_carcinogen_risk", columnDefinition = "double(0, 0) UNSIGNED")
+    private Double nonCarcinogenRisk;
+
+    @Column(name = "carcinogen_risk", columnDefinition = "double(0, 0) UNSIGNED")
+    private Double carcinogenRisk;
+
+}
--- src/main/java/ua/dolphiedude/ecomon/entity/Substance.java
+++ src/main/java/ua/dolphiedude/ecomon/entity/Substance.java
@@ -1,11 +1,16 @@
 package ua.dolphiedude.ecomon.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 @Entity
+@Table(name = "substance")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,13 +20,36 @@ public class Substance {
     @Column(name = "id")
     private Long id;
 
+    @NotNull
     @Column(name = "name")
     private String name;
+
     @Column(name = "units")
     private String units;
 
     @Column(name = "mass_consumption")
     private Double massConsumption;
+
+    @NotNull
+    @Column(name = "reference_concentration")
+    private Double refConcentration;
+
+    @Column(name = "slope_factor")
+    private Double slopeFactor;
+    @NotNull
+    @Column(name = "threshold_limit_value", nullable = false)
+    private Double tlv;
+
+    @NotNull
+    @Column(name = "mass_flow_rate", nullable = false)
+    private Integer massFlowRate;
+
+    @Column(name = "toxicity_class")
+    private Integer toxicityClass;
+
+    @OneToOne(mappedBy = "substance")
+    private Tax tax;
+
 
     @Override
     public String toString() {
